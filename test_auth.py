70a88997021341636c05fae89064b18550472804
Preethi M N <preethimn9741@gmail.com>
2026-01-13 10:55:03 +0530

    Create test_auth.py

---
+++ test_auth.py
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Auth App</title>
+</head>
+<body>
+<h2>User Authentication</h2>
+
+<input id="username" placeholder="Username">
+<input id="password" type="password" placeholder="Password">
+
+<button onclick="register()">Register</button>
+<button onclick="login()">Login</button>
+
+<pre id="output"></pre>
+
+<script src="/static/script.js"></script>
+</body>
+</html>
